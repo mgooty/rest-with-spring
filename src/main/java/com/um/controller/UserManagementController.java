9b78681ce80952c59fbf4f381b05338606bd6bec
Mithun Gooty <mithun.gooty@gmail.com>
2023-11-15 22:53:10 +0530

    Added hateoas support

--- src/main/java/com/um/controller/UserManagementController.java
+++ src/main/java/com/um/controller/UserManagementController.java
@@ -4,6 +4,9 @@ import com.um.resource.UserResource;
 import com.um.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,12 +26,27 @@ public class UserManagementController {
     }
 
     @GetMapping
-    public List<UserResource> getAll() {
-        return userService.getAll();
+    public CollectionModel<List<UserResource>> getAll() {
+        List<UserResource> allUsers = userService.getAll();
+        CollectionModel entityModel = CollectionModel.of(allUsers);
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAll());
+        entityModel.add(link.withRel("create-user"));
+
+        return entityModel;
     }
 
     @GetMapping(value = "/greetings", produces = "application/json;charset=utf-8")
     public String greetings() {
         return userService.greetings();
+    }
+
+    @GetMapping(value = "/greetings", headers = "x-api-version=1")
+    public String versioningV1() {
+        return "header based version #1";
+    }
+
+    @GetMapping(value = "/greetings", headers = "x-api-version=2")
+    public String versioningV2() {
+        return "header based version #2";
     }
 }
